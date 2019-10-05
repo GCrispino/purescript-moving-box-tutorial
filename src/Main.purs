2512c7a74dc14d0cebe18aa20a45f6220980cbdf
Gabriel Crispino <gabriel.crispino@hotmail.com>
2019-10-04 21:15:28 -0300

    Make box move to the right

--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,7 @@ module Main where
 import Prelude
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Ref (Ref, new, read, write)
 import Effect.Console (log)
 
 import Web.DOM.Document (Document, createElement) as DOM
@@ -16,6 +17,10 @@ import Web.HTML.HTMLElement as HTML.HTMLElement
 
 foreign import setStyleProp :: String -> String -> DOM.Element.Element -> Effect Boolean
 
+type State = { position :: Number
+    , rafId :: HTML.Window.RequestAnimationFrameId
+}
+
 createBoxElement :: String -> DOM.Document -> Effect DOM.Element.Element
 createBoxElement id document = do
     boxEl <- DOM.createElement "div" document
@@ -27,14 +32,30 @@ createBoxElement id document = do
     _ <- setStyleProp "background" "#ff4242" boxEl
     pure boxEl
 
-execFrame :: Effect Unit
-execFrame = do
-        log "Request animation frame!!"
-        w <- HTML.window
-        -- Call next frame
-        animationFrameId <- HTML.Window.requestAnimationFrame (execFrame) w
-        pure unit
+moveBox :: DOM.Element.Element -> Ref State -> Effect Unit
+moveBox el stateRef = do
+    -- Read state
+    state <- read stateRef
+    
+    -- Move box
+    let distValPx = state.position
+        distStr   = (show distValPx) <> "px"
 
+    _ <- setStyleProp "transform" ( "translate(" <> distStr <> ", 0)" ) el
+
+    w <- HTML.window
+    width <- HTML.Window.innerWidth w
+
+    -- Call next frame
+    animationFrameId <- HTML.Window.requestAnimationFrame (moveBox el stateRef) w
+
+    -- Update state
+    write { position: distValPx + 9.0
+        , rafId: animationFrameId
+    } stateRef
+
+execFrame :: DOM.Element.Element -> Ref State -> Effect Unit
+execFrame el stateRef = moveBox el stateRef
 
 main :: Effect Unit
 main = do
@@ -44,6 +65,14 @@ main = do
   mBody <- HTML.body d
   defaultElem <- (DOM.createElement "span" (HTML.toDocument d))
 
+  -- Create frame that does nothing just to get default frame id
+  defaultId <- (HTML.Window.requestAnimationFrame (pure unit) w)
+
+  -- Default state
+  stateRef <- new { position: 0.0
+    , rafId: defaultId
+  }
+
   let b = case mBody of 
         Nothing -> DOM.Element.toNode (defaultElem)
         Just b'  -> HTML.HTMLElement.toNode b'
@@ -52,6 +81,6 @@ main = do
   newBody <- DOM.appendChild (DOM.Element.toNode boxEl) b
 
   frameId <- HTML.Window.requestAnimationFrame (
-    execFrame
+    execFrame boxEl stateRef
   ) w
   pure unit
